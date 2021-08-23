5c73cf12875e4703ca6f0f2214de75d7e171da8f
Finn Voigtländer <finn.voigtlaender@googlemail.com>
2021-08-23 18:19:24 +0200

    test: :white_check_mark: new tests for isroman, romanize and deromanize

--- index.test.js
+++ index.test.js
@@ -1,30 +1,108 @@
 const romanize = require('./index.js')
 
-describe('validator', () => {
+describe('validate non roman values', () => {
   const INVALID = [
-    undefined, null, 0, 123, "", "0", "123", "ABC", [], {}, () => {}, "IIII", "IIIV", "IVV"
+    undefined, null, 0, 123, "", "0", "123", "ABC", new Array(), {}, () => {}, "IIII", "IIIV", "IVV"
   ]
 
-  const VALID = [
-    "I", "III", "VII", "MMXXI", "i", "mmxxi"
-  ]
-
-  INVALID.forEach(VAR => {
-    test(`expect "${VAR}" to be invalid`, () => {
-      expect(!romanize.validroman(VAR))
+  INVALID.forEach((VAR, I) => {
+    test(`N°${String(I).padStart(2, "0")} is NOT roman: "${VAR}"`, () => {
+      expect(!romanize.isRoman(VAR))
     })
   });
-
-  VALID.forEach(VAR => {
-    test(`expect "${VAR}" to be valid`, () => {
-      expect(romanize.validroman(VAR))
+})
+  
+describe('validate roman numbers', () => {
+  const VALID = [
+    "I", "i", "III", "VII", "mmmm", "LLLII", "lllII", "MMXXI", "mmxxi", "cdMI", "MLI"
+  ]
+  VALID.forEach((VAR, I) => {
+    test(`N°${String(I).padStart(2, "0")} is roman: "${VAR}"`, () => {
+      expect(romanize.isRoman(VAR))
     })
   });
 })
 
+describe('romanize arabic numbers', () => {
+  const ARABIC = [
+    [1, "I"],
+    [2, "II"],
+    [5, "V"],
+    [9, "IX"],
+    [30, "XXX"],
+    [69, "LXIX"],
+    [99, "XCIX"],
+    [210, "CCX"],
+    [420, "CDXX"],
+    [2021, "MMXXI"],
+    [3999, "MMMCMXCIX"],
+    [-123, "-CXXIII"],
+  ]
 
-describe('romanize', () => {
-  test('expect 1 in roman to be "I"', () => {
-    expect(romanize.romanize(1)).toBe("I")
-  })
+  const FAILSAFE = [
+    [null, NaN],
+    [undefined, NaN],
+    [0, NaN],
+    [false, NaN],
+    [true, NaN],
+    [new Array(), NaN],
+    [() => {}, NaN],
+    [new Object(), NaN],
+    [new Date(), NaN],
+    [new Set(), NaN],
+  ]
+
+  ARABIC.forEach((VAR, I) => {
+    test(`N°${String(I + 1).padStart(2, "0")} ${VAR[0]} in roman is "${VAR[1]}"`, () => {
+      expect(romanize.romanize(VAR[0])).toBe(VAR[1])
+    })
+  });
+
+  FAILSAFE.forEach((VAR, I) => {
+    test(`N°${String(I + 1 + ARABIC.length).padStart(2, "0")} ${VAR[1]} returned for ${VAR[0]}`, () => {
+      expect(romanize.romanize(VAR[0])).toBe(VAR[1])
+    })
+  });
+})
+
+describe('deromanize roman numbers', () => {
+  const ROMAN = [
+    [1, "I"],
+    [2, "II"],
+    [5, "V"],
+    [9, "IX"],
+    [30, "XXX"],
+    [69, "LXIX"],
+    [99, "XCIX"],
+    [210, "CCX"],
+    [420, "CDXX"],
+    [2021, "MMXXI"],
+    [3999, "MMMCMXCIX"],
+    [-123, "-CXXIII"],
+  ]
+
+  const FAILSAFE = [
+    [null, NaN],
+    [undefined, NaN],
+    [0, NaN],
+    [false, NaN],
+    [true, NaN],
+    [new Array(), NaN],
+    [() => {}, NaN],
+    [new Object(), NaN],
+    [new Date(), NaN],
+    [new Set(), NaN],
+  ]
+
+  ROMAN.forEach((VAR, I) => {
+    test(`N°${String(I + 1).padStart(2, "0")} ${VAR[0]} in arabic is "${VAR[1]}"`, () => {
+      expect(romanize.romanize(VAR[0])).toBe(VAR[1])
+    })
+  });
+
+  FAILSAFE.forEach((VAR, I) => {
+    test(`N°${String(I + 1 + ROMAN.length).padStart(2, "0")} ${VAR[1]} returned for ${VAR[0]}`, () => {
+      expect(romanize.romanize(VAR[0])).toBe(VAR[1])
+    })
+  });
 })
